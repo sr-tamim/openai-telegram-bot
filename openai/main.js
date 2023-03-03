52b55d47e30c1abd4f751d8b63ee54ffe396e77f
unknown <saifur.rahman.tamim1@gmail.com>
2023-03-03 19:26:21 +0600

    bug fix

--- openai/main.js
+++ openai/main.js
@@ -1,7 +1,7 @@
 const { OpenAIApi } = require("openai")
 const openai_config = require("./openai.config");
 
-async function generateChatResponse(message, userID) {
+async function generateChatResponse(message, user) {
     const openai = new OpenAIApi(openai_config);
 
     const completion = await openai.createChatCompletion({
@@ -11,7 +11,7 @@ async function generateChatResponse(message, userID) {
             { role: "user", content: message }
         ],
         max_tokens: 128,
-        user: userID || ""
+        user
     });
     return (completion.data.choices[0].message.content);
 }
