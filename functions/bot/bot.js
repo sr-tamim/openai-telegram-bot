7ecf4aaf7df6d87d09de96335c829707e118ec08
unknown <saifur.rahman.tamim1@gmail.com>
2023-03-03 15:54:45 +0600

    openai integration

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -1,13 +1,26 @@
 const { Telegraf } = require("telegraf")
+const { generateChatResponse } = require("../../openai/main")
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.start(ctx => {
     console.log("Received /start command")
     try {
-        return ctx.reply("Hi")
+        return ctx.reply("Hi, this OpenAI_Bot_BD, ready to chat with you.")
     } catch (e) {
         console.error("error in start action:", e)
         return ctx.reply("Error occured")
+    }
+})
+
+bot.command("echo", async (ctx) => {
+    if(ctx.message.via_bot){
+        return ctx.reply("Sorry! I don't reply bots.");
+    }
+    try {
+        const response = await generateChatResponse(ctx.message.text);
+        return ctx.reply(response);
+    }catch(error){
+        return ctx.reply("Error occured");
     }
 })
 
---
+++ openai/main.js
@@ -0,0 +1,13 @@
+import { OpenAIApi } from "openai";
+import { openai_config } from "./openai.config";
+
+export async function generateChatResponse(message) {
+    const openai = new OpenAIApi(openai_config);
+
+    const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: message }],
+        max_tokens: 128
+    });
+    return (completion.data.choices[0].message.content);
+}
