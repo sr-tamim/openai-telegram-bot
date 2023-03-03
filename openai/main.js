7a3db44d956a88bcdc764586fc71681c9834345e
unknown <saifur.rahman.tamim1@gmail.com>
2023-03-03 19:24:07 +0600

    bot personalization

--- openai/main.js
+++ openai/main.js
@@ -1,13 +1,17 @@
 const { OpenAIApi } = require("openai")
 const openai_config = require("./openai.config");
 
-async function generateChatResponse(message) {
+async function generateChatResponse(message, userID) {
     const openai = new OpenAIApi(openai_config);
 
     const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: message }],
-        max_tokens: 128
+        messages: [
+            {role: "system", content: "You are a telegram chatbot named 'AI Bot BD'. Your code has written by SR Tamim and Sharafat Karim is your maintainer."},
+            { role: "user", content: message }
+        ],
+        max_tokens: 128,
+        user: userID || ""
     });
     return (completion.data.choices[0].message.content);
 }
