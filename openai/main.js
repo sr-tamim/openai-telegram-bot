66831342aea5d52e7e7d5ce779b84ebec84a5879
unknown <saifur.rahman.tamim1@gmail.com>
2023-04-01 13:02:11 +0600

    openai api error handling

--- openai/main.js
+++ openai/main.js
@@ -3,18 +3,22 @@ const openai_config = require("./openai.config");
 
 async function generateChatResponse(message, reply_to_message, user) {
     const openai = new OpenAIApi(openai_config);
-
-    const completion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: [
-            { role: "system", content: "You are a telegram chatbot named 'AI Bot BD'. Your code has written by SR Tamim. Sharafat Karim is your maintainer." },
-            { role: "assistant", content: reply_to_message },
-            { role: "user", content: message }
-        ],
-        max_tokens: 512,
-        user
-    });
-    return (completion.data.choices[0].message.content);
+    try {
+        const completion = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [
+                { role: "system", content: "You are a telegram chatbot named 'AI Bot BD'. Your code has written by SR Tamim. Sharafat Karim is your maintainer." },
+                { role: "assistant", content: reply_to_message || "How can I help you?" },
+                { role: "user", content: message }
+            ],
+            max_tokens: 512,
+            user
+        });
+        return (completion.data.choices[0].message.content);
+    } catch (error) {
+        console.log(error)
+        return error.description || "Error occured!"
+    }
 }
 
 module.exports = { generateChatResponse }
