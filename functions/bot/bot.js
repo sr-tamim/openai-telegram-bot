62e46538cb6755f663967bf9182c01971e69b394
SR Tamim <saifur.rahman.tamim1@gmail.com>
2023-07-01 23:29:15 +0600

    message queue setup

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -2,9 +2,37 @@ const { Telegraf } = require("telegraf")
 require("dotenv").config()
 const { generateChatResponse } = require("../../openai/main")
 const bot = new Telegraf(process.env.BOT_TOKEN)
-
 const allowedGroups = process.env.GROUP_ID.toString().split(',')
 
+// message queue to avoid openai api rate limit
+const messageQueue = []
+let lastReplySent = Date.now()
+const delay = 20000 // as per openai docs, i can send 3 requests per minute https://platform.openai.com/docs/guides/rate-limits/what-are-the-rate-limits-for-our-api
+
+
+/* ======= helper functions ======= */
+const sendResponse = async () => {
+    if (messageQueue.length === 0) return console.log("No message in queue")
+    const ctx = messageQueue[0]
+    const now = Date.now()
+    const timeGap = now - lastReplySent
+    if (timeGap > delay) {
+        const response = await generateChatResponse(ctx.message.text, ctx.message?.reply_to_message?.text,
+            ctx.message?.from?.username || ctx.message?.from?.id?.toString());
+        messageQueue.shift()
+        lastReplySent = Date.now()
+        ctx.reply(response, {
+            parse_mode: "Markdown",
+            reply_to_message_id: ctx.message?.message_id,
+            allow_sending_without_reply: true,
+            reply_markup: { force_reply: true, selective: true }
+        });
+    }
+    if (messageQueue.length > 0) return setTimeout(() => sendResponse(), delay - timeGap)
+}
+
+
+/* ======= bot actions ======= */
 bot.start(async ctx => {
     console.log("Received /start command")
     try {
@@ -34,15 +62,8 @@ bot.on("message", async (ctx) => {
         if (ctx.message?.reply_to_message?.from?.id?.toString() !== process.env.BOT_ID.toString()) return
 
         ctx.telegram.sendChatAction(ctx.message.chat.id, "typing")
-
-        const response = await generateChatResponse(ctx.message.text, ctx.message?.reply_to_message?.text,
-            ctx.message?.from?.username || ctx.message?.from?.id?.toString());
-        return ctx.reply(response, {
-            parse_mode: "Markdown",
-            reply_to_message_id: ctx.message?.message_id,
-            allow_sending_without_reply: true,
-            reply_markup: { force_reply: true, selective: true }
-        });
+        messageQueue.push(ctx)
+        return messageQueue.length === 1 ? sendResponse() : null
     } catch (error) {
         console.log(error)
         return ctx.reply("Error occured");
