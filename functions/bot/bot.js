874825aae81e9fb0262b32a0e0e334e3381ef1e1
Tamim <saifur.rahman.tamim1@gmail.com>
2023-07-13 15:40:49 +0600

    code & little feature improvements

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -16,38 +16,67 @@ const imageDelay = 12000 // as per openai docs, i can send 5 image generation re
 
 
 /* ======= helper functions ======= */
+const checkGroup = (ctx) => {
+    const isAllowed = allowedGroups.includes(ctx.message?.chat?.id.toString())
+    if (!isAllowed) {
+        ctx.reply("I am not allowed to reply outside specific groups. Contact with my maintainers if you want to test my capabilities. \nDeveloper > @sr_tamim \nMaintainer > @SharafatKarim");
+    };
+    return isAllowed;
+}
 const sendResponse = async () => {
     if (messageQueue.length === 0) return console.log("No message in queue")
-    const ctx = messageQueue[0]
+    const { ctx, loadingMsg } = messageQueue[0]
     const now = Date.now()
     const timeGap = now - lastReplySent
     if (timeGap > delay) {
-        const response = await generateChatResponse(ctx.message.text, ctx.message?.reply_to_message?.text,
-            ctx.message?.from?.username || ctx.message?.from?.id?.toString());
-        messageQueue.shift()
-        lastReplySent = Date.now()
-        ctx.reply(response, {
-            parse_mode: "Markdown",
-            reply_to_message_id: ctx.message?.message_id,
-            allow_sending_without_reply: true,
-            reply_markup: { force_reply: true, selective: true }
-        });
+        try {
+            ctx.telegram.sendChatAction(ctx.message.chat.id, "typing")
+
+            // generate response from openai
+            const response = await generateChatResponse(ctx.message.text, ctx.message?.reply_to_message?.text,
+                ctx.message?.from?.username || ctx.message?.from?.id?.toString());
+
+            ctx.deleteMessage(loadingMsg.message_id) // delete "generating response..." message
+            await ctx.reply(response, { // send response
+                parse_mode: "Markdown", // to parse markdown in response
+                reply_to_message_id: ctx.message?.message_id, // to reply to user's the message
+                allow_sending_without_reply: true, // send message even if user's message is not found
+                reply_markup: { force_reply: true, selective: true } // to force user to reply to this message
+            });
+        } catch (e) {
+            console.log(e)
+            ctx.reply("Error occured!")
+        } finally {
+            messageQueue.shift() // remove first element from queue}
+            lastReplySent = Date.now() // update last reply sent time
+        }
     }
+    // if there are more messages in queue, call this function again after delay
     if (messageQueue.length > 0) return setTimeout(() => sendResponse(), delay - timeGap)
 }
+
+
+// send image response function
 const sendImageResponse = async () => {
     if (imageQueue.length === 0) return console.log("No image in queue")
-    const ctx = imageQueue[0]
+    const { ctx, loadingMsg } = imageQueue[0]
     const now = Date.now()
     const timeGap = now - lastImageSent
     if (timeGap > imageDelay) {
         try {
+            ctx.telegram.sendChatAction(ctx.message.chat.id, "upload_photo") // send "uploading photo" action
+
+            // prepare prompt
             const prompt = ctx.message.text.includes("/image@OpenAI_BD_bot") ?
                 (ctx.message.text.replace("/image@OpenAI_BD_bot", "").trim())
                 : (ctx.message.text.replace("/image", "").trim())
+
+            // generate image from openai
             const response = await generateImageResponse(prompt, ctx.message?.from?.username || ctx.message?.from?.id?.toString());
-            lastImageSent = Date.now()
-            ctx.replyWithPhoto(response, {
+
+            ctx.deleteMessage(loadingMsg.message_id) // delete "generating image..." message
+
+            await ctx.replyWithPhoto(response, {  // send image
                 reply_to_message_id: ctx.message?.message_id,
                 allow_sending_without_reply: true
             });
@@ -56,6 +85,7 @@ const sendImageResponse = async () => {
             ctx.reply("Error occured!")
         } finally {
             imageQueue.shift()
+            lastImageSent = Date.now()
         }
     }
     if (imageQueue.length > 0) return setTimeout(() => sendImageResponse(), imageDelay - timeGap)
@@ -66,13 +96,11 @@ const sendImageResponse = async () => {
 bot.start(async ctx => {
     console.log("Received /start command")
     try {
-        if (!allowedGroups.includes(ctx.message?.chat?.id.toString())) {
-            return ctx.reply("I am not allowed to reply outside specific groups. Contact with my maintainers if you want to test my capabilities. \nDeveloper > @sr_tamim \nMaintainer > @SharafatKarim");
-        };
+        if (!checkGroup(ctx)) return; // check if bot is allowed to reply in this group
+
         return ctx.reply("Hi, this is *AI Bot BD*, ready to chat with you. \nReply to my message to start chatting...", {
             parse_mode: "Markdown",
             allow_sending_without_reply: true,
-            reply_to_message_id: ctx.message?.message_id,
             reply_markup: { force_reply: true, selective: true }
         })
     } catch (e) {
@@ -86,9 +114,8 @@ bot.command("image", async ctx => {
         return ctx.reply("Sorry! I don't reply bots.");
     }
     try {
-        if (!allowedGroups.includes(ctx.message?.chat?.id.toString())) {
-            return ctx.reply("I am not allowed to reply outside specific groups. Contact with my maintainers if you want to test my capabilities. \nDeveloper > @sr_tamim \nMaintainer > @SharafatKarim");
-        };
+        if (!checkGroup(ctx)) return; // check if bot is allowed to reply in this group
+
         const hasPrompt = ctx.message.text.includes("/image@OpenAI_BD_bot") ?
             (!!ctx.message.text.replace("/image@OpenAI_BD_bot", "").trim())
             : (!!ctx.message.text.replace("/image", "").trim())
@@ -99,8 +126,12 @@ bot.command("image", async ctx => {
             reply_markup: { force_reply: true, selective: true }
         })
 
-        ctx.telegram.sendChatAction(ctx.message.chat.id, "upload_photo")
-        imageQueue.push(ctx)
+        // send a loading message
+        const loadingMsg = await ctx.reply("Generating image...", {
+            reply_to_message_id: ctx.message?.message_id,
+            allow_sending_without_reply: true,
+        })
+        imageQueue.push({ ctx, loadingMsg })
         return imageQueue.length === 1 ? sendImageResponse() : null
     } catch (error) {
         console.log(error)
@@ -113,15 +144,16 @@ bot.on("message", async (ctx) => {
         return ctx.reply("Sorry! I don't reply bots.");
     }
     try {
-        if (!allowedGroups.includes(ctx.message?.chat?.id.toString())) {
-            return ctx.reply("I am not allowed to reply outside specific groups. Contact with my maintainers if you want to test my capabilities. \nDeveloper > @sr_tamim \nMaintainer > @SharafatKarim");
-        };
+        if (!checkGroup(ctx)) return; // check if bot is allowed to reply in this group
 
         // message must be a reply of this bot's message
         if (ctx.message?.reply_to_message?.from?.id?.toString() !== process.env.BOT_ID.toString()) return
 
-        ctx.telegram.sendChatAction(ctx.message.chat.id, "typing")
-        messageQueue.push(ctx)
+        const loadingMsg = await ctx.reply("Generating response...", {
+            reply_to_message_id: ctx.message?.message_id,
+            allow_sending_without_reply: true,
+        })
+        messageQueue.push({ ctx, loadingMsg })
         return messageQueue.length === 1 ? sendResponse() : null
     } catch (error) {
         console.log(error)
--- openai/main.js
+++ openai/main.js
@@ -3,22 +3,18 @@ const openai_config = require("./openai.config");
 
 async function generateChatResponse(message, reply_to_message, user) {
     const openai = new OpenAIApi(openai_config);
-    try {
-        const completion = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo",
-            messages: [
-                { role: "system", content: "You are a telegram chatbot named 'AI Bot BD'. Your code has written by SR Tamim. Sharafat Karim is your maintainer." },
-                { role: "assistant", content: reply_to_message || "How can I help you?" },
-                { role: "user", content: message }
-            ],
-            max_tokens: 512,
-            user
-        });
-        return (completion.data.choices[0].message.content);
-    } catch (error) {
-        console.log(error)
-        return error.description || "Error occured!"
-    }
+
+    const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+            { role: "system", content: "You are a telegram chatbot named 'AI Bot BD'. Your code has written by SR Tamim. Sharafat Karim is your maintainer." },
+            { role: "assistant", content: reply_to_message || "How can I help you?" },
+            { role: "user", content: message }
+        ],
+        max_tokens: 512,
+        user
+    });
+    return (completion.data.choices[0].message.content);
 }
 
 async function generateImageResponse(prompt, user) {
