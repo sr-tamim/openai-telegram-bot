5e9618adf3ce8985e85b52d74a080defd8151c91
Tamim <saifur.rahman.tamim1@gmail.com>
2023-10-30 23:42:20 +0600

    openai api expired message

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -6,7 +6,14 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 const allowedGroups = process.env.GROUP_ID.toString().split(',')
 
 bot.start(async ctx => {
-    console.log("Received /start command")
+    const greetings = `**Hello, ${ctx.message?.from?.first_name ?
+        `${ctx.message?.from?.first_name} ${ctx.message?.from?.last_name||""}`
+        : ctx.message?.from?.username || "Homo Sapiens! **\n"}`;
+
+    return ctx.reply(greetings + "Our free access of OpenAI API has been expired. So, we are not able to provide you the service anymore. \n\nIf you want to use this bot, please contact with my maintainers. \nDeveloper > @sr_tamim \nMaintainer > @SharafatKarim \n\nThank you for using this bot. \n\n[View My Source Code](https://github.com/sr-tamim.openai-telegram-bot)", {
+        parse_mode: "Markdown",
+        disable_web_page_preview: true
+    });
     try {
         if (!allowedGroups.includes(ctx.message?.chat?.id.toString())) {
             return ctx.reply("I am not allowed to reply outside specific groups. Contact with my maintainers if you want to test my capabilities. \nDeveloper > @sr_tamim \nMaintainer > @SharafatKarim");
@@ -25,6 +32,14 @@ bot.on("message", async (ctx) => {
     if (ctx.message.via_bot) {
         return ctx.reply("Sorry! I don't reply bots.");
     }
+    const greetings = `**Hello, ${ctx.message?.from?.first_name ?
+        `${ctx.message?.from?.first_name} ${ctx.message?.from?.last_name||""}`
+        : ctx.message?.from?.username || "Homo Sapiens! **\n"}`;
+
+    return ctx.reply(greetings + "Our free access of OpenAI API has been expired. So, we are not able to provide you the service anymore. \n\nIf you want to use this bot, please contact with my maintainers. \nDeveloper > @sr_tamim \nMaintainer > @SharafatKarim \n\nThank you for using this bot. \n\n[View My Source Code](https://github.com/sr-tamim.openai-telegram-bot)", {
+        parse_mode: "Markdown",
+        disable_web_page_preview: true
+    });
     try {
         if (!allowedGroups.includes(ctx.message?.chat?.id.toString())) {
             return ctx.reply("I am not allowed to reply outside specific groups. Contact with my maintainers if you want to test my capabilities. \nDeveloper > @sr_tamim \nMaintainer > @SharafatKarim");
